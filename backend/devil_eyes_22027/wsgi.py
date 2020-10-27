d5184e8f50e1b77fc0148695c3752e28cf2a4143
Crowdbotics <team@crowdbotics.com>
2020-10-27 20:33:15 +0000

    Update models

--- backend/devil_eyes_22027/wsgi.py
+++ backend/devil_eyes_22027/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devil_eyes_22027.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devil_eyes_22027.settings")
 
 application = get_wsgi_application()
